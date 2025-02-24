78a59870f96cc6cb02c23d3d3dccd3e88c64b66f
LoganFerreira <logan.ferreira@etu.unilim.fr>
2025-02-24 22:13:36 +0100

    final commit

--- server/index.js
+++ server/index.js
@@ -3,29 +3,28 @@
 const Glue = require('@hapi/glue');
 const Exiting = require('exiting');
 const Manifest = require('./manifest');
+const consumeMessages = require('../lib/services/messageConsumer');
 
 exports.deployment = async ({ start } = {}) => {
-
     const manifest = Manifest.get('/', process.env);
     const server = await Glue.compose(manifest, { relativeTo: __dirname });
 
     if (start) {
         await Exiting.createManager(server).start();
         server.log(['start'], `Server started at ${server.info.uri}`);
+        consumeMessages(server).catch(console.error);
         return server;
     }
 
     await server.initialize();
-
     return server;
 };
 
 if (require.main === module) {
-
     exports.deployment({ start: true });
 
     process.on('unhandledRejection', (err) => {
-
-        throw err;
+        console.error('Unhandled rejection:', err);
+        process.exit(1);
     });
 }
